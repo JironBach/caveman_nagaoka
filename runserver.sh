56a82b2e07b67955033ec58b6a2dbb965b00c230
JironBach <junshimo2@gmail.com>
2020-02-17 10:06:44 +0900

    move position of clackup

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PATH="~/bin:~/caveman_nagaoka/bin:$PATH:."
-APP_ENV=development clackup --address 0.0.0.0 --port 5000 app.lisp
+APP_ENV=development bin/clackup --address 0.0.0.0 --port 5000 app.lisp
 
